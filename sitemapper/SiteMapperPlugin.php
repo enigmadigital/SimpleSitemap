e47acffa370df5505d44d290aa8afb3425c4f92f
Robert Anderson <robert.anderson@xodigital.com.au>
2014-12-10 11:03:30 +1100

    generate site map for all entries

--- sitemapper/SiteMapperPlugin.php
+++ sitemapper/SiteMapperPlugin.php
@@ -1,39 +1,32 @@
 <?php namespace Craft;
 
 
-class SitemapperPlugin extends BasePlugin
+class SiteMapperPlugin extends BasePlugin
 {
-
 	public function getName()
 	{
-		return 'Sitemapper';
+		return 'SiteMapper';
 	}
-
 
 	public function getVersion()
 	{
 		return '0.0.1';
 	}
 
-
 	public function getDeveloper()
 	{
 		return 'XO Digital';
 	}
-
 
 	public function getDeveloperUrl()
 	{
 		return 'http://xodigital.com.au';
 	}
 
-
 	public function registerSiteRoutes()
 	{
 		return array(
-			'sitemap.xml' => array('action' => 'sitemapper/map/index'),
-			'v1/sitemap.xml' => array('action' => 'sitemapper/map/index')
+			'sitemap.xml' => array('action' => 'siteMapper/index'),
 		);
 	}
-
 }
---
+++ sitemapper/controllers/SiteMapperController.php
@@ -0,0 +1,31 @@
+<?php namespace Craft;
+
+
+class SiteMapperController extends BaseController
+{
+	protected $allowAnonymous = true;
+
+	public function actionIndex()
+	{
+		$xml = new \SimpleXMLElement(
+			'<?xml version="1.0" encoding="UTF-8"?>' .
+			'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"/>'
+		);
+
+		$criteria = craft()->elements->getCriteria(ElementType::Entry);
+		$criteria->limit = null;
+
+		foreach ($criteria as $entry)
+		{
+			$url = $xml->addChild('url');
+			$url->addChild('loc', $entry->url);
+			$url->addChild('lastmod', $entry->dateUpdated->format(\DateTime::W3C));
+			$url->addChild('priority', $entry->uri == '__home__' ? 0.75 : 0.5);
+		}
+
+		HeaderHelper::setContentTypeByExtension('xml');
+		ob_start();
+		echo $xml->asXML();
+		craft()->end();
+	}
+}
